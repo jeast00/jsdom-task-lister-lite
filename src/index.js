9aea8bce9ac83a2de5ca6deff594df23d6e870b8
jeast00 <38165949+jeast00@users.noreply.github.com>
2021-02-24 10:05:20 -0500

    finished deliverables; working on stretch deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const form = document.getElementById('create-task-form');
+  const input = document.getElementById('new-task-description');
+  // const list = document.getElementById('list');
+  const tasks = document.getElementById('tasks');
+
+  // console.log(form);
+  // console.log(input);
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let inputValue = input.value;
+    // console.log(inputValue);
+
+    // create li for list tasks
+    const li = document.createElement('li');
+    li.innerText = input.value;
+
+    // append the li to the ul element
+    tasks.appendChild(li);
+
+    form.reset();
+
+
+  });
 });
